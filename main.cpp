9e0387d578f5d6b02006ca734b8a6d0bf060b5e9
YU-LIN-LIN <nophy0428@gmail.com>
2020-04-01 17:49:53 +0800

    origin

--- main.cpp
+++ main.cpp
@@ -4,11 +4,16 @@
 
 
 uLCD_4DGL uLCD(D1, D0, D2);
+PwmOut PWM1(D6);
+AnalogIn Ain(A0);
+AnalogOut Aout(DAC0_OUT);
+float ADCdata;
 
 
 int main()
 
 {
+    int i;
 
       uLCD.printf("\n106061237\n"); //Default Green on black text
 
@@ -18,5 +23,22 @@ int main()
     uLCD.line(0, 127, 0, 0, WHITE);
 
       wait(1);
+
+      while(1){
+
+        
+
+        for (i=0; i<128; i++) {
+            PWM1.period(0.1);
+
+            PWM1 = Ain;
+
+            wait(0.1);
+            pc.printf("%1.3f\r\n", ADCdata);
+            Aout = ADCdata;
+        }
+      }
+
+
 
 }
